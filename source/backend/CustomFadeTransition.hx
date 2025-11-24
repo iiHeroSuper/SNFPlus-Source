83c25f483947bbfd4092c79a6f1d0b0e4f4c6656
Yeey79 <casi.mos.05@gmail.com>
2025-11-24 17:35:45 +0200

    Update CustomFadeTransition.hx

--- source/backend/CustomFadeTransition.hx
+++ source/backend/CustomFadeTransition.hx
@@ -9,64 +9,68 @@ import backend.MusicBeatSubstate;
 
 class CustomFadeTransition extends MusicBeatSubstate
 {
-	public static var finishCallback:Void->Void;
-	
-	var isTransIn:Bool = false;
-	var transSprite:FlxSprite;
+    public static var finishCallback:Void->Void;
+    
+    var isTransIn:Bool = false;
+    var transSprite:FlxSprite;
 
-	public function new(duration:Float, isTransIn:Bool)
-	{
-		this.isTransIn = isTransIn;
-		super();
-	}
+    public function new(duration:Float, isTransIn:Bool)
+    {
+        this.isTransIn = isTransIn;
+        super();
+    }
 
-	override function create()
-	{
-		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+    override function create()
+    {
+        cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 
-		transSprite = new FlxSprite();
-		transSprite.frames = Paths.getSparrowAtlas('fade/s1fade'); 
+        transSprite = new FlxSprite();
+        transSprite.frames = Paths.getSparrowAtlas('fade/s1fade'); 
 
-		transSprite.animation.addByPrefix('close', 'fade in', 30, false);
-		transSprite.animation.addByPrefix('open', 'fade out', 30, false);
+        transSprite.animation.addByPrefix('close', 'fade in', 30, false);
+        transSprite.animation.addByPrefix('open', 'fade out', 30, false);
 
-		transSprite.screenCenter();
-		transSprite.scrollFactor.set();
+        transSprite.screenCenter();
+        transSprite.scrollFactor.set();
 
-		if(isTransIn)
-			transSprite.animation.play('close');
+        // FIX: Added braces { } so the callback is part of the condition
+        if(isTransIn) 
+        {
+            transSprite.animation.play('close');
 
-			transSprite.animation.finishCallback = function(name:String) {
-				close();
-			};
-		else 
-			{
-			transSprite.animation.play('open');
+            transSprite.animation.finishCallback = function(name:String) {
+                close();
+            };
+        }
+        else 
+        {
+            transSprite.animation.play('open');
 
-			transSprite.animation.finishCallback = function(name:String) {
-				close();
-			};
-		}
+            transSprite.animation.finishCallback = function(name:String) {
+                close();
+            };
+        }
 
-		add(transSprite);
-		super.create();
-	}
+        add(transSprite);
+        super.create();
+    }
 
-	override function close():Void
-	{
-		super.close();
+    override function close():Void
+    {
+        super.close();
 
-		if(finishCallback != null)
-		{
-			finishCallback();
-			finishCallback = null;
-		}
-	}
+        if(finishCallback != null)
+        {
+            finishCallback();
+            finishCallback = null;
+        }
+    }
 
-	override function destroy()
-	{
-		if(finishCallback != null)
-			finishCallback = null;
-		}
-		super.destroy();
+    override function destroy()
+    {
+        if(finishCallback != null) {
+            finishCallback = null;
+        }
+        super.destroy();
+    }
 }
